291963409929a0ae742da8d2ffbd1f3320893f26
Rick Brown <ricky.brown.00@gmail.com>
2021-06-19 21:15:43 +0100

    Initial Commit

---
+++ crypto-tracker-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  crypto-tracker-ios
+//
+//  Created by Rick Brown on 19/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ crypto-tracker-ios/crypto_tracker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_tracker_iosApp.swift
+//  crypto-tracker-ios
+//
+//  Created by Rick Brown on 19/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct crypto_tracker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
