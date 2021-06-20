88d460dfe9ecd5abd5940e9870f1f0687ca5a08b
Rick Brown <ricky.brown.00@gmail.com>
2021-06-20 17:24:23 +0100

    removes print statements

--- crypto-tracker-ios/Services/CoinImageService.swift
+++ crypto-tracker-ios/Services/CoinImageService.swift
@@ -27,10 +27,8 @@ class CoinImageService {
   private func getCoinImage() {
     if let image = fileManager.getImage(imageName: imageName, folderName: folderName) {
       self.image = image
-      print("image loaded from filemanager")
     } else {
       downloadCoinImage()
-      print("image downloaded")
     }
   }
   
